0d0fa984c0bd68a764519d995d2bbe40c3d602a3
rexcare367 <rex.care.367@gmail.com>
2020-12-28 19:54:41 +0530

    authenticated method added to redirect the logged in user to a page based on their role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -20,13 +21,16 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->hasRole('superadministrator') || $user->hasRole('admin')) {
+            return redirect()->route('admin');
+        }
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+        if($user->hasRole('user')) {
+            return redirect()->route('user');
+        }
+    }
 
     /**
      * Create a new controller instance.
